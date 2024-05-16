629f169ff23f17db29240ca3f25aada01e1339d1
Sviatoslav <113264589+Me1nychuk@users.noreply.github.com>
2024-05-16 20:15:19 +0300

    Task completed.

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,72 @@
 import contactsService from "../services/contactsServices.js";
+export const getAllContacts = async (req, res) => {
+  contactsService
+    .listContacts()
+    .then((contacts) => res.status(200).json(contacts))
+    .catch((err) => res.status(500).json("Internal Server Error"));
+};
 
-export const getAllContacts = (req, res) => {};
+export const getOneContact = (req, res) => {
+  const { id } = req.params;
+  contactsService
+    .getContactById(id)
+    .then((contact) => {
+      if (contact == null) {
+        res.status(404).json({ message: "Contact not found" });
+      }
+      res.status(200).json(contact);
+    })
+    .catch((err) => res.status(500).json("Internal Server Error"));
+};
 
-export const getOneContact = (req, res) => {};
+export const deleteContact = (req, res) => {
+  const { id } = req.params;
+  contactsService
+    .removeContact(id)
+    .then((contact) => {
+      if (contact == null) {
+        res.status(404).json({ message: "Contact not found" });
+      }
+      res.status(204);
+    })
+    .catch((err) => res.status(500).json("Internal Server Error"));
+};
 
-export const deleteContact = (req, res) => {};
+export const createContact = (req, res) => {
+  const { name, email, phone, favortie } = req.body;
+  contactsService
+    .addContact(name, email, phone, favortie)
+    .then((contact) => {
+      res.status(201).json(contact);
+    })
+    .catch((err) => res.status(500).json("Internal Server Error"));
+};
 
-export const createContact = (req, res) => {};
+export const updateContact = (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone, favortie } = req.body;
 
-export const updateContact = (req, res) => {};
+  contactsService
+    .updateContact(id, favortie, name, email, phone)
+    .then((contact) => {
+      if (contact == null) {
+        res.status(404).json({ message: "Contact not found" });
+      }
+      res.status(201).json(contact);
+    })
+    .catch((err) => res.status(500).json("Internal Server Error"));
+};
+
+export const updateContactFavorite = (req, res) => {
+  const { id } = req.params;
+  const { favorite } = req.body;
+  contactsService
+    .updateContactFavorite(id, favorite)
+    .then((contact) => {
+      if (contact == null) {
+        res.status(404).json({ message: "Not found" });
+      }
+      res.status(200).json(contact);
+    })
+    .catch((err) => res.status(500).json("Internal Server Error"));
+};
